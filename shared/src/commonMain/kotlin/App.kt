d74e1a942459db3f43fbd750efe76f210336b1ed
Xiaowei Chen <xiaoqianniu@gmail.com>
2023-06-11 10:48:29 -0400

    use composable function to pass login data and dismiss card,clear the textFields

--- shared/src/commonMain/kotlin/App.kt
+++ shared/src/commonMain/kotlin/App.kt
@@ -1,15 +1,12 @@
 @file:OptIn(ExperimentalMaterialApi::class)
 
-import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -18,26 +15,16 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Button
 import androidx.compose.material.Card
-import androidx.compose.material.DismissState
-import androidx.compose.material.DrawerValue
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.ModalDrawer
 import androidx.compose.material.Scaffold
-import androidx.compose.material.SnackbarDefaults.backgroundColor
 import androidx.compose.material.Text
-import androidx.compose.material.TextButton
 import androidx.compose.material.TextField
 import androidx.compose.material.TopAppBar
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Close
-import androidx.compose.material.rememberDrawerState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -48,7 +35,6 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import kotlinx.coroutines.launch
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
 import ui.theme.AppTheme
@@ -96,7 +82,10 @@ fun App() {
 
 }
 
-/* add registration form background color*/
+/* add registration form background color
+* add logo image clickable and display messages
+* modify the textFields in one line
+* todo: notification*/
 @OptIn(ExperimentalResourceApi::class)
 @Composable
 fun myLoginForm() {
@@ -245,8 +234,6 @@ fun myLoginForm() {
         Button(
             onClick = {
                 showCard.value = true
-                emailValue.value = ""
-                passwordValue.value = ""
             },
             enabled = !emailValue.value.isEmpty() && !passwordValue.value.isEmpty()
         ) {
@@ -255,43 +242,50 @@ fun myLoginForm() {
 
     }
     if (showCard.value) {
-        Card(
-            modifier = Modifier.padding(top = 16.dp).fillMaxSize(),
-            elevation = 10.dp,
+        loginInfoDisplay(emailValue.value,passwordValue.value,
+            onBackClicked = { showCard.value = false
+            emailValue.value = ""
+            passwordValue.value = ""}
+        )
 
-            ) {
-            Column(
-                modifier = Modifier.padding(16.dp)
-            ) {
-                Text(
-                    text = "Welcome back! ",
-                    style = MaterialTheme.typography.h5,
-                    modifier = Modifier.padding(bottom = 8.dp)
-                )
-                Text(
-                    text = "User Information",
-                    style = MaterialTheme.typography.h6,
-                    modifier = Modifier.padding(bottom = 8.dp)
-                )
-                Text(
-                    text = "Email: ${emailValue.value}",
-                    style = MaterialTheme.typography.body1,
-                    modifier = Modifier.padding(bottom = 4.dp)
-                )
-                Text(
-                    text = "Password: ${passwordValue.value}",
-                    style = MaterialTheme.typography.body1
-                )
-                Button(onClick = {
-                    showCard.value = false
+    }
+}
 
-                }) {
-                    Text("Back", fontSize = 20.sp)
-                }
+/* loginInfoDisplay method with three parameters:
+for displaying the login information: email,password and dismiss the card and clear the textFields*/
+@Composable
+fun loginInfoDisplay(email: String, password:String,onBackClicked: () -> Unit){
+    Card(
+        modifier = Modifier.padding(top = 16.dp).fillMaxSize(),
+        elevation = 10.dp,
+        ) {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text(
+                text = "Welcome back! ",
+                style = MaterialTheme.typography.h5,
+                modifier = Modifier.padding(bottom = 8.dp)
+            )
+            Text(
+                text = "User Information",
+                style = MaterialTheme.typography.h6,
+                modifier = Modifier.padding(bottom = 8.dp)
+            )
+            Text(
+                text = "Email: $email",
+                style = MaterialTheme.typography.body1,
+                modifier = Modifier.padding(bottom = 4.dp)
+            )
+            Text(
+                text = "Password: $password",
+                style = MaterialTheme.typography.body1
+            )
+            Button(onClick = { onBackClicked()
+            }) {
+                Text("Back", fontSize = 20.sp)
             }
         }
     }
 }
-
-
 expect fun getPlatformName(): String
